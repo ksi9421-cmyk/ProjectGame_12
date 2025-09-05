02ed82ca25441529589aaacfef2915d2d3a41019
kangminyong <minyong1998@naver.com>
2025-09-05 23:12:21 +0900

    [Shop/Gold] Shop/Gold
    개인 브랜치에 초본 올립니다

---
+++ ProjectGame_12/Shop.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Gold.h"
+
+using namespace std;
+
+struct Item // 데이터묶음
+{
+	string name; // 템 이름
+	int price; // 템 가격
+};
+
+class Shop
+{
+private:
+	vector<Item> Itemlist; // 상점판매목록
+
+
+public:
+
+	Shop();
+	void buyItem(Gold &playerGold); // 플레이어 돈 참조해서 차감
+};

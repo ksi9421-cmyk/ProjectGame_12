6af527cc7acbf21a20ea410b8446041cca783a46
ksi9421-cmyk <ksi9421@gmail.com>
2025-09-05 23:33:28 +0900

    [Initial]
    h/cpp만 만들어 놓은 버전

--- ProjectGame_12/Monster.h
+++ ProjectGame_12/Monster.h
@@ -1,5 +1,19 @@
 #pragma once
-class Monster
+#include <string>
+using namespace std;
+
+
+class Monster           //순수 가상함수로 기능할것
 {
+protected:
+	string name;
+	int HP;
+	int Power;
+	int Lev;
+
+public:
+	Monster(string name);
+
+
 };
 

5ba5949345ae46069694064c7ba1f5d2b766dd85
knc0511 <76583907+knc0511@users.noreply.github.com>
2025-09-09 12:17:47 +0900

    Player

--- ProjectGame_12/Player.cpp
+++ ProjectGame_12/Player.cpp
@@ -1,1 +1,59 @@
 #include "Player.h"
+
+using namespace std;
+
+Player::Player(string nickname, int level, int health, int attack, int gold, int exp) :
+	Name(nickname), Level(1), Health(200), Attack(30), Gold(0), Exp(0) {}
+
+void Player::levelUp()
+{
+	if (Exp >= 100)
+	{
+		Level++;
+		Health += 50;
+		Attack += 10;
+		Exp = 0;
+
+		cout << "레벨 업! 현재 레벨: " << Level << endl;
+		cout << "공격력: " << Attack << ", 체력: " << Health << endl << endl;
+	}
+}
+
+void Player::printPlayerStatus() const {
+	cout << "----- 플레이어 상태 -----" << endl;
+	cout << "       공격력: " << Attack << endl;
+	cout << "           HP: " << Health << endl;
+	cout << "          Lv.: " << Level << endl;
+	cout << "       소지금: " << Gold << endl;
+	cout << "=========================" << endl;
+}
+
+void Player::takeDamage(int Damage)
+{
+	Health -= Damage;
+
+	if (Health < 0)
+		Health = 0;
+
+	cout << "-" << Damage << endl;
+}
+
+void Player::openInventory() {
+	
+}
+
+
+int Player::getHealth()
+{
+	return Health;
+}
+
+int Player::getAttack()
+{
+	return Attack;
+}
+
+int Player::getGold()
+{
+	return Gold;
+}
---
+++ ProjectGame_12/main.cpp
@@ -0,0 +1,10 @@
+/*#include "Player.h"
+using namespace std;
+
+int main()
+{
+	cout << "닉네임을 입력해 주세요" << endl;
+
+
+	return 0;
+}
